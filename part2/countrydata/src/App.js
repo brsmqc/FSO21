82579b9fae6f4cca0f073882cd7d0778ce464ee1
Barry Still <brsmqc@gmail.com>
2022-01-11 10:03:05 -0300

    displaying countries works, filter does not

--- part2/countrydata/src/App.js
+++ part2/countrydata/src/App.js
@@ -1,9 +1,46 @@
-import React from 'react'
-import './App.css'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Country = ({country}) => {
+  return(
+    <div>
+      {country.name.common}
+    </div>
+  )
+}
+
+const Display = ({countries, filter}) => {
+  console.log(countries)
+  return(
+      <div>
+        {countries.map(country =>
+          <Country key={country.name.common} country={country} />  
+        )}
+      </div>
+  ) 
+}
 
 function App() {
+  const [countries, setCountries] = useState([])
+  const [filter, setFilter] = useState('')
+  
+  const handleFilter = (event) => {
+    setFilter(event.target.value)
+  }
+  
+  useEffect(() => {
+    axios
+      .get(`https://restcountries.com/v3.1/all`)
+      .then(response => {
+        setCountries(response.data)
+        console.log(response.data)
+      })
+  }, [])
+  
   return (
     <div>
+      find countries <input value={filter} onChange={handleFilter} />
+      <Display countries={countries} filter={filter} />
     </div>
   )
 }
